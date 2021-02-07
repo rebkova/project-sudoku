c2e33909fbc0eec8920ed44ecd317731dbc33fd1
Rebkova <rebkova@gmail.com>
2021-02-07 18:49:54 +0100

    commented out unused code

--- frontend/src/components/LeaderBoardItem.js
+++ frontend/src/components/LeaderBoardItem.js
@@ -1,5 +1,5 @@
 import React from "react"
-import styled from 'styled-components/macro'
+// import styled from 'styled-components/macro'
 
 export const LeaderBoardItem = ({ username, time }) => {
 
