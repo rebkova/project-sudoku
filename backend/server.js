696a147dcef148ffa94ad303319973eb9c7d062e
Rebkova <rebkova@gmail.com>
2021-02-12 11:31:06 +0100

    cleaned up backend

--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,12 @@ import mongoose from 'mongoose';
 import crypto from 'crypto';
 import bcrypt from 'bcrypt';
 
-// import LeaderBoard from '../backend/models/leaderboard'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-sudoku"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-//define a schema
+
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
@@ -34,42 +33,35 @@ const userSchema = new mongoose.Schema({
   }
 })
 
-//pre mongoose-hook: allows hashing after we validate but before we save to DB
-//hook is applied to the schema, not the model()
 userSchema.pre('save', async function (next) {
 
-  //the "this" variable has all the data from the userSchema and we can access it
-  //"this" is a keyword reffering to "the object you're currently in"
   const user = this;
 
-  //if the pw is not modified, we don't rehash it
   if (!user.isModified('password')) {
     return next();
   }
 
-  //modified pw: Hash the password, after the validation of sign up details
   const salt = bcrypt.genSaltSync();
 
-  //we're updating pw in the schema before it's saved
   user.password = bcrypt.hashSync(user.password, salt);
 
   next();
 });
 
-//create a model
+
 const User = mongoose.model('User', userSchema);
 
 const LeaderBoard = mongoose.model('LeaderBoard', {
   username: { type: String },
   time: { type: Number },
-  //perhaps createdAt not neccessary?
+
   createdAt: {
     type: Date,
     default: Date.now
   }
 })
 
-//clear the database
+
 if (process.env.RESET_DATABASE) {
   const clearDatabase = async () => {
     await User.deleteMany({}, () => {
@@ -84,15 +76,12 @@ if (process.env.RESET_DATABASE) {
   clearDatabase()
 }
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+
 const port = process.env.PORT || 8080;
 const app = express();
 import listEndpoints from 'express-list-endpoints';
 
-//Function that expects the user's accesstoken and validate access to restricted endpoints
+
 const authenticateUser = async (request, response, next) => {
 
   const user = await User.findOne({ accessToken: request.header('Authorization') });
@@ -105,11 +94,11 @@ const authenticateUser = async (request, response, next) => {
   }
 };
 
-// Add middlewares to enable cors and json body parsing
+
 app.use(cors());
 app.use(bodyParser.json());
 
-//middleware f() -> checks if we have a connection with the server (1 = connected)
+
 app.use((request, response, next) => {
   if (mongoose.connection.readyState === 1) {
     next();
@@ -118,7 +107,6 @@ app.use((request, response, next) => {
   }
 });
 
-//Main API page
 app.get('/', (request, response) => {
 
   if (response) {
@@ -130,28 +118,25 @@ app.get('/', (request, response) => {
 
 
 //SIGN UP ENDPOINT
-//This endpoint registers the user & puts it in the database
 app.post('/users', async (request, response) => {
   try {
     const { username, email, password } = request.body;
 
-    //validation happens here + saving to the DB!
     const user = await new User({
       username,
       email,
       password,
     }).save();
 
-    response.status(200).json({ userID: user._id }); //Sign up success & user is not able to be logged in right after that
+    response.status(200).json({ userID: user._id });
   }
   catch (err) {
-    response.status(400).json({ message: 'Sorry, could not create user', errors: err }); // Sign up error
+    response.status(400).json({ message: 'Sorry, could not create user', errors: err });
   }
 })
 
 
 //LOGIN ENDPOINT
-//This endpoint expects username and password from already existing users
 app.post('/sessions', async (request, response) => {
   try {
 
@@ -161,17 +146,16 @@ app.post('/sessions', async (request, response) => {
     if (user && bcrypt.compareSync(password, user.password)) {
       response
         .status(200)
-        .json({ userId: user._id, accessToken: user.accessToken }); //Success
+        .json({ userId: user._id, accessToken: user.accessToken });
     } else {
-      throw 'Incorrect username or password'; //If user that's signed up is trying to login but login details are incorrect
+      throw 'Incorrect username or password';
     }
   } catch (err) {
-    response.status(404).json({ error: 'Sorry, user does not exist' }); //If a user that's not signed up is trying to login
+    response.status(404).json({ error: 'Sorry, user does not exist' });
   }
 });
 
 
-//Restricted endpoint, only accessible after user has logged in with valid username and access token
 app.get('/sessions/:id/profile', authenticateUser);
 app.get('/sessions/:id/profile', (request, response) => {
 
@@ -180,8 +164,6 @@ app.get('/sessions/:id/profile', (request, response) => {
   response.status(201).json(userMessage)
 });
 
-//LEADERBOARD -> no need for authentication?
-// app.get('/leaderboard', authenticateUser);
 app.get('/leaderboard', async (request, response) => {
 
   try {
@@ -199,6 +181,7 @@ app.post('/leaderboard', async (request, response) => {
 
   try {
     const username = request.body.username
+
     const time = request.body.time
 
     const leaderBoard = new LeaderBoard({ username, time })
